d7a0a73e1d494c6049d651fa9b11721488862fb0
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-14 15:41:23 -0400

    Add: design-in-memory-file-system - submission-1

---
+++ Data Structures & Algorithms/design-in-memory-file-system/submission-1.py
@@ -0,0 +1,46 @@
+import bisect
+
+class FileSystem:
+
+    def __init__(self):
+        # self.hashMap = {"/": []}
+        self.paths = collections.defaultdict(list)
+        self.files = collections.defaultdict(str)
+        
+
+    def ls(self, path: str) -> List[str]:
+        if path in self.files:
+            return [path.split("/")[-1]]
+        else:
+            return self.paths[path]
+        
+
+    def mkdir(self, path: str) -> None:
+        directories = path.split("/")
+
+        for idx in range(1, len(directories)):
+            cur = "/".join(directories[:idx]) or "/"
+
+            if (cur not in self.paths or directories[idx] not in self.paths[cur]):
+                bisect.insort(self.paths[cur], directories[idx])
+
+        
+
+    def addContentToFile(self, filePath: str, content: str) -> None:
+        if (filePath not in self.files):
+            self.mkdir(filePath)
+        
+        self.files[filePath] += content
+        
+
+    def readContentFromFile(self, filePath: str) -> str:
+        return self.files[filePath]
+        
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.ls(path)
+# obj.mkdir(path)
+# obj.addContentToFile(filePath,content)
+# param_4 = obj.readContentFromFile(filePath)
